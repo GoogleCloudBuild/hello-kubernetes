7946c96e91a18422fe8f9926a40893626d242df4
Nick Franklin <n.o.franklin@gmail.com>
2018-09-28 13:27:56 -0400

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -12,8 +12,8 @@ func TestGreet(t *testing.T) {
 		want string
 	}{
 		{
-			arg:  "Brian",
-			want: "Hello Brian!",
+			arg:  "Jose",
+			want: "Hello Jose!",
 		}, {
 			arg:  "Mary",
 			want: "Hello Mary!",
